4b072fffb563c9ac7188a33f4c3db30114bde5b3
Sam Osterkil <pisketch@gmail.com>
2013-02-25 13:31:38 -0500

    Progress on parsing, improved regex, string formatters

--- Postbox.py
+++ Postbox.py
@@ -1,45 +1,70 @@
 import sys
-# import re
+import re
 from ircutils import bot
 
 
 class IRCBot(bot.SimpleBot):
-    global min_len
-    min_len = 5
-    print("Starting...")
+    print('Starting...')
+    MIN_LEN = 5
+    CANON_REGEX = '(?:Postbox|postbox)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)\s(?P<action><.+>)?\s([?{<y>^.!]+).{0,3}'
 
     def on_channel_message(self, event):
         message = event.message.split()
-        print(message[0].upper)
-        if message[0].upper == "EXITNOW":
-            print("Recived Exit Order")
-            if event.source.upper() == "PISKETCH" or event.source.upper() == "PAOANI":
+        if message[0].upper == 'EXITNOW':
+            print('Recived Exit Order')
+            if event.source.upper() == 'PISKETCH' or event.source.upper() == 'PAOANI':
                 sys.exit()
-        elif len(message[0].upper()) > min_len:
-            print("Message length over min_len: " + event.message + ' (From ' + event.source + ')')
+        elif len(message[0].upper()) > MIN_LEN:
+            print('Message over MIN_LEN, sending to parser: %s (from %s') % (event.message, event.source)
+            start_parse(self, event)
             self.send_message(event.target, event.message)
 
     def on_private_message(self, event):
-        print("Echoing private message from " + event.source + ": " + event.message)
+        print('Echoing private message from ' + event.source + ': ' + event.message)
         self.send_message(event.source, event.message)
 
+    def get_ops(self, event):
+        print "getops executing"
+        self.execute('NAMES', '#postroom')
+        if event.command == 'RPL_NAMREPLY':
+            print re.findall(r'\b@\w+', event.message)
+
     def on_join(self, event):
+        self.execute("PRIVMSG", 'PISKETCH', trailing='execute works!')
+        self.get_ops()
         if event.source == self.nickname:
-            print("Joined channel.")
-            self.send_message("NickServ", "identify bukkpass101")
+            print('Joined channel.')
+            self.send_message('NickServ', 'identify bukkpass101')
         else:
-            self.send_message(event.target, "Hello, " + event.source + ".")
+            self.send_message(event.target, 'Hello, ' + event.source + '.')
 
-    def parse(message):
-        # We'll handle checking for assignment/triggers in here
-        # http://www.debuggex.com/?re=%28%3F%3APostbox%7Cpostbox%29%28%3F%3A%5B%2C%3A%5D%5Cs%7C%5Cs%29%28%5Cw%2B%29%5Cs%28is%7Care%7C%3C.%2B%3E%29%5Cs%28%5Cw%2B%7C%5Cd%2B%29.&str=Postbox%3A+eggplants+%3Creply%3E+eggs.
-        if message.match('(?:Postbox|postbox)(?:[,:]\s|\s)(\w+)\s(is|are|<.+>)\s(\w+|\d+).'):
-            print("Matched a command!")
+    def parse_assignment(self, event):
+        print 'parse_assignment '
+        # regex at http://cl.ly/26112w0z0142 for analysis
+        elements = re.match(CANON_REGEX, event.message).groupdict
+        x = elements.get('x')
+        verb = elements.get('verb') + ' ' + elements.get('action')
+        y = elements.get('y')
+        self.send_message(event.target, 'Okay %s, %s %s %s.') % (event.source, x, verb, y)
+
+    def parse_trigger(self, event):
+        # tbd
+        print 'parse_trigger called'
+
+    def start_parse(self, event):
+        # We'll handle checking for assignment/triggers in here.
+        # If the message looks like an assignment, pass it to an appropriate method
+        # If it looks like it could trigger something, likewise
+        # http://cl.ly/1R301X0c2E1Q
+        if re.match(CANON_REGEX, event.message):
+            parse_assignment(self, event)
         else:
-            print("Not a command.")
-        return message
+            # If parse_addressed was called, we should check if it's a trigger
+            parse_trigger(self, event)
+        return event.message
 
-if __name__ == "__main__":
-    echo = IRCBot("Postbox")  # Name it
-    echo.connect("irc.freenode.net", channel=["#testbox"])  # connect
+
+if __name__ == '__main__':
+    echo = IRCBot('Postbox')  # Name it
+    echo.connect('irc.rizon.net', channel=['#postroom', '#postroomctrl'])  # connect
     echo.start()  # start
