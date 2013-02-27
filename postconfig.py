4a9edd3a7723493c2d9027a6017605a22c50512c
Sam Osterkil <pisketch@gmail.com>
2013-02-27 09:36:19 -0500

    Moved config vars into separate file

---
+++ postconfig.py
@@ -0,0 +1,4 @@
+# Configuration variables, imported into Postbox.py.
+MIN_LEN = 5
+CANON_REGEX = '(?:Postbox|postbox)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)(?P<action>\s<.+>)?\s(?P<y>[^.!]+).{0,3}'
+OPS = []
