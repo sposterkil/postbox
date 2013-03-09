1a2d0fd492b4f6bf7acef4ef93045ae4004199a5
Sam Osterkil <pisketch@gmail.com>
2013-03-09 00:54:59 -0600

    add Dict for triggers, for now

--- postconfig.py
+++ postconfig.py
@@ -1,4 +1,8 @@
+import re
+
+
 # Configuration variables, imported into Postbox.py.
 MIN_LEN = 5
-CANON_REGEX = '(?:Postbox|postbox)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)(?P<action>\s<.+>)?\s(?P<y>[^.!]+).{0,3}'
+CANON_REGEX = re.compile('(?:Postbox|postbox)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)(?P<action>\s<.+>)?\s(?P<y>[^.!]+).{0,3}')
 OPS = []
+TRIGGERS = {}
