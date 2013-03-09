8d3d818b6c4fb846d2fad92df594fe8067742f85
Sam Osterkil <pisketch@gmail.com>
2013-03-09 01:00:12 -0600

    Delete renamed file

--- postconfig.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-# Configuration variables, imported into Postbox.py.
-MIN_LEN = 5
-CANON_REGEX = re.compile('(?:Postbox|postbox)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)(?P<action>\s<.+>)?\s(?P<y>[^.!]+).{0,3}')
-OPS = []
-TRIGGERS = {}
