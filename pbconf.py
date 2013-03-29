f73953dcd983fde2d37c9d7038b9a12d9dd13ac0
Sam Osterkil <pisketch@gmail.com>
2013-03-29 00:20:09 -0400

    Updated config vars

--- pbconf.py
+++ pbconf.py
@@ -2,7 +2,9 @@ import re
 
 
 # Configuration variables, imported into Postbox.py.
-MIN_LEN = 5
-CANON_REGEX = re.compile('(?:Postbox|postbox)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)(?P<action>\s<.+>)?\s(?P<y>[^.!]+).{0,3}')
-OPS = []
-TRIGGERS = {}
+NICK = 'King_of_Brohan'
+CHANNElS = [['ridersofbrohan', 'irc.freenode.net']]  # Add more channels on more servers by adding a list in the form ['channel', 'server']
+MIN_LEN = 2
+CANON_REGEX = re.compile('(?:%s.capitalize()|%s)(?:[,:]\s|\s)(?P<x>[^.!]+)\s(?P<verb>is|are)(?P<action>\s<.+>)?\s(?P<y>[^.!]+).{0,3}' % (NICK, NICK))
+OPS = []  # Move these into Postbox.py and pickle them!
+TRIGGERS = {}  # This one too
